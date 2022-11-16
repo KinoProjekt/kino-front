539eda878a5781f113237f20aa4324c984971c33
staaaa <stachubozych@gmail.com>
2022-11-16 18:38:46 +0100

    router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,10 +6,17 @@ const PageNotFound = () => <Navigate to={'/'} />
 const Router = () => {
   return (
     <BrowserRouter>
+      <p>navbar</p>
       <Routes>
         <Route path={'/'} element={<h1>Strona Główna</h1>} />
+        <Route path={'/repertuar'} element={<h1>Strona testowa</h1>} />
+        <Route path={'/wydarzenia'} element={<h1>Strona testowa</h1>} />
+        <Route path={'/promocje'} element={<h1>Strona testowa</h1>} />
+        <Route path={'/wiadomosci'} element={<h1>Strona testowa</h1>} />
+        <Route path={'/film/:filmName'} element={<h1>Strona testowa</h1>} />
         <Route path={'*'} element={<PageNotFound />} />
       </Routes>
+      <p>navbar</p>
     </BrowserRouter>
   )
 }
