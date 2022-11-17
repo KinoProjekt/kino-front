96ae0da4a094b629965cd37cdee3d4e4cc3518cd
qirele <kirilczikzik@gmail.com>
2022-11-17 18:05:27 +0100

    start movie entries styling

--- src/components/FilmList/FilmList.jsx
+++ src/components/FilmList/FilmList.jsx
@@ -1,6 +1,7 @@
 import './FilmList.scss'
 import Line from '../../common/Line/Line.jsx'
 import SearchInput from '../../common/SearchInput/SearchInput'
+import MovieEntry from './MovieEntry'
 
 const FilmList = () => {
   return (
@@ -10,8 +11,44 @@ const FilmList = () => {
         <SearchInput/>
       </div>
       <Line />
+
+
+      <div className="rep-body">
+        {movies.map(movie => {
+          return <MovieEntry key={movie.title} movie={movie}/>
+        })}
+      </div>
+
     </div>
   )
 }
+
+
+const movies = [
+  {
+    title: "Avatar: Istota Wody",
+    availableSeats: 1,
+    premierDate: "14/11/2022",
+    premierTime: "20:00"
+  },
+  {
+    title: "Dzika noc",
+    availableSeats: 17,
+    premierDate: "14/11/2022",
+    premierTime: "18:00"
+  },
+  {
+    title: "Bella i Sebastian: Nowe Pokolenie",
+    availableSeats: 2,
+    premierDate: "15/11/2022",
+    premierTime: "20:00"
+  },
+  {
+    title: "Kierunek księżyc",
+    availableSeats: 32,
+    premierDate: "15/11/2022",
+    premierTime: "18:00"
+  },
+]
 
 export default FilmList
---
+++ src/components/FilmList/MovieEntry.jsx
@@ -0,0 +1,24 @@
+import {AiOutlinePlus} from 'react-icons/ai';
+import Line from '../../common/Line/Line.jsx';
+import './MovieEntry.scss';
+
+const MovieEntry = ({movie}) => {
+  return(<>
+    <Line />  
+    <div className="rep-movie">
+      <AiOutlinePlus className="rep-movie-icon" />
+      <div className="rep-movie-1">
+        <p>{movie.title}</p>
+        <p>{movie.availableSeats} wolnych miejsc</p>
+      </div>
+      <div className='rep-movie-2'>
+        <p>{movie.premierDate}</p>
+        <p>{movie.premierTime}</p>
+      </div>
+    </div>
+    <Line />  
+  
+  </>)
+}
+
+export default MovieEntry;
