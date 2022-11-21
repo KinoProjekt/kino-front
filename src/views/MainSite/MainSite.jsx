cc9ab0aeb2db12b96aa83a6c8a6d542b0e4a02fc
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2022-11-21 16:18:14 +0100

    Added scroll to top button

--- src/views/MainSite/MainSite.jsx
+++ src/views/MainSite/MainSite.jsx
@@ -1,10 +1,12 @@
 // import s from './MainSite.module.scss'
+import { ScrollButton } from '../../common/ScrollButton/ScrollButton'
 import {FilmList, EventList, PromotionList} from '../../components/'
 
 const MainSite = () => {
   return (
     <>
       <FilmList />
+      <ScrollButton />
       <EventList />
       <PromotionList />
     </>
